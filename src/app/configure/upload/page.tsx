f0d1e60246185d69aee2fce5012c0bd3b185f8ae
thanhthu42 <jvanime212@gmail.com>
2024-09-13 13:07:40 +0700

    init

---
+++ src/app/configure/upload/page.tsx
@@ -0,0 +1,123 @@
+"use client";
+import { Progress } from "@/components/ui/progress";
+import { useUploadThing } from "@/lib/uploadthing";
+import { cn } from "@/lib/utils";
+import { Image, Loader2, MousePointerSquareDashed } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useTransition } from "react";
+import { useState } from "react";
+import Dropzone, { FileRejection } from "react-dropzone";
+import { useToast } from "@/hooks/use-toast"
+
+const Page = () => {
+  const [isDragOver, setIsDragOver] = useState<boolean>(false);
+  const [uploadProgress, setUploadProgress] = useState<number>(0);
+  const router = useRouter();
+  const { toast } = useToast()
+
+  const { startUpload, isUploading } = useUploadThing("imageUploader", {
+    onClientUploadComplete: ([data]) => {
+      const configId = data.serverData.configId;
+      startTransition(() => {
+        router.push(`/configure/design?id=${configId}`);
+      });
+    },
+    onUploadProgress: (progress) => {
+      setUploadProgress(progress);
+    },
+  });
+
+  const onDropRejected = (rejectFiles: FileRejection[]) => {
+    const [file] = rejectFiles;
+
+    setIsDragOver(false);
+    toast({
+     title: `${file.file.type} is not supported`,
+     description: "Please upload a PNG, JPG, or JPEG file",
+     variant: "default",
+    });
+
+  };
+  const onDropAccepted = (acceptedFiles: File[]) => {
+    startUpload(acceptedFiles, { configId: undefined });
+    setIsDragOver(false);
+  };
+
+  const [isPending, startTransition] = useTransition();
+  return (
+    <div
+      className={cn(
+        "relative h-full flex-1 my-16 w-full rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:rounded-2xl flex justify-center flex-col items-center",
+        {
+          "ring-blue-900/25 bg-blue-900/10": isDragOver,
+        }
+      )}
+    >
+      <div
+        className="relative flex flex-1 flex-col
+     items-center justify-center w-full"
+      >
+        <Dropzone
+          onDropRejected={onDropRejected}
+          onDropAccepted={onDropAccepted}
+          accept={{
+            "image/jpeg": [".jpeg"],
+            "image/jpg": [".jpg"],
+            "image/png": [".png"],
+          }}
+          onDragEnter={() => setIsDragOver(true)}
+          onDragLeave={() => setIsDragOver(false)}
+        >
+          {({ getRootProps, getInputProps }) => (
+            <div
+              {...getRootProps()}
+              className="flex flex-col justify-center items-center flex-1 w-full h-full"
+              {...getRootProps()}
+            >
+              <input {...getInputProps()} />
+              {isDragOver ? (
+                <MousePointerSquareDashed className="h-6 w-6 text-zinc-500 mb-2" />
+              ) : isUploading || isPending ? (
+                <Loader2 className="animate-spin h-6 w-6 text-zinc-500 mb-2" />
+              ) : (
+                <Image className="h-6 w-6 text-zinc-500 mb-2" />
+              )}
+              <div
+                className="flex flex-col justify-center mb-2 text-sm
+              text-zinc-700"
+              >
+                {isUploading ? (
+                  <div className="flex flex-col items-center">
+                    <p>Uploading...</p>
+                    <Progress
+                      value={uploadProgress}
+                      className="mt-2 w-40 h-2 bg-gray-300"
+                    />
+                  </div>
+                ) : isPending ? (
+                  <div className="flex flex-col items-center">
+                    <p>Redirecting, please wait...</p>
+                  </div>
+                ) : isDragOver ? (
+                  <p>
+                    <span className="font-semibold">Drop file</span> to upload
+                  </p>
+                ) : (
+                  <p>
+                    <span className="font-semibold">Click to upload</span> or
+                    drag and drop
+                  </p>
+                )}
+              </div>
+              {isPending ? null : (
+                <p className="text-xs text-zinc-500">PNG, JPG, JPEG</p>
+              )}
+            </div>
+          )}
+        </Dropzone>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import { Toaster } from "@/components/ui/toaster";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,7 +31,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Navbar />
+        <main className="flex flex-col min-h-[calc(100vh-3.5rem-1px)]">
+          <div className="flex-1 flex flex-col h-full">{children}</div>
+          <Footer />
+        </main>
+        <Toaster />
       </body>
     </html>
   );
