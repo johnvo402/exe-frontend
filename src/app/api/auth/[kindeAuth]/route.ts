cb40dc355139707b5d9129e15c6e310df949166f
thanhthu42 <jvanime212@gmail.com>
2024-10-23 03:42:02 +0700

    .

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,3 +1,12 @@
 import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
 
-export const GET = handleAuth();
+export const GET = handleAuth(() => {
+    return new Response('Hello, Next.js!', {
+        status: 200,
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+            'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+        },
+    });
+});
